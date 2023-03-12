6a27bbc0f68cfa7da13a4c408ade3e04bcee8265
jacques <jacques.vida01@student.csulb.edu>
2023-03-12 12:47:29 -0700

    Fixed spacing on cards and fixed navar; added link

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const prjbtn = document.getElementById("projects");
+const homebtn = document.getElementById("home");
+const contactbtn = document.getElementById("contact");
+const resumebtn = document.getElementById("resume");
 
 prjbtn.addEventListener("click", function(e) {
     window.location.href="./projects.html";
+});
+
+homebtn.addEventListener("click", function(e) {
+    window.location.href="./index.html";
+});
+
+contactbtn.addEventListener("click", function(e) {
+    window.location.href="./contact.html";
+});
+
+resumebtn.addEventListener("click", function(e) {
+    window.location.href="./resume.html";
 });
